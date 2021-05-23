ed0adaadca434f84905d17f4981802905d587366
hariszaf <haris.zafr@gmail.com>
2021-05-24 01:04:25 +0300

    build CREST input after Swarm

---
+++ pema_docker_image/scripts/buildAllTableFile.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+
+awk -F "\t" 'BEGIN {OFS = "\t"} { $2=""; $NF=""; print }' asvs_contingency_table.tsv | \
+sed 's/linearized.dereplicate_//g ; s/.merged.fastq//g ; s/OTU/#OTU_ID/g; s/^\([0-9].*\)/Otu\1/g ; s/\t\t/\t/g ; s/_/ /g'  > tmp
+
+awk -F '\t' '{print $1}' asvs_contingency_table.tsv | sed 's/^\([0-9].*\)/Otu\1/g ; s/OTU/#OTU_ID/g' > firstCol
+
+head -1 asvs_contingency_table.tsv | awk 'BEGIN {OFS = "\t"}{for(i=3;i<=NF-1;++i) printf $i"\t"}' |  sed 's/linearized.dereplicate_//g ; s/.merged.fastq//g' > header 
+
+while read line; do grep -v OTU |  awk -v b=2 -v e=3 'BEGIN{FS=OFS="\t"} {for (i=b;i<=e;i++) printf "%d%s", $i, (i<e ? OFS : ORS)}' > OUTPUT ;done < tmp
+
+awk '1' header OUTPUT > samples
+
+paste -d "\t" firstCol samples  > allTab.tsv
+
+rm tmp firstCol header samples OUTPUT
+
+
+# Map Swarm ids to Otu numbers
+
+sed '1i\\' asvs_representatives_all_samples.fasta > tml 
+mv tml asvs_representatives_all_samples.fasta
+
+while read line; do grep ">" |  sed 's/>//g ; s/_.*//g' | xargs -I {} grep {} asvs_contingency_table.tsv | awk 'BEGIN {OFS="\t"} {print $1 "\t" $2}'; done < asvs_representatives_all_samples.fasta  > pairs
+
+tail -n +2 asvs_representatives_all_samples.fasta > tml
+mv tml asvs_representatives_all_samples.fasta
+
+
+awk -F "\t" {'print ">Otu"$1'} pairs > otus
+cat asvs_representatives_all_samples.fasta | grep -v ">" > seqs
+
+paste -d \\n otus seqs  > asvs_representatives_all_samples.fasta
+
+rm pairs otus seqs 
+
