d9a93c42db578e5c41f340b978f60f0a4aa34299
hariszaf <haris.zafr@gmail.com>
2021-05-28 11:04:46 +0300

    anaylysis dir @ develop @ oxygen

---
+++ analysis_directory/custom_ref_db/README.md
@@ -0,0 +1,10 @@
+You only need this directory, in case you need to use your own reference database.
+
+If that is the case, **you need to keep the name of this directory exactly as it is**, i.e ```custom_ref_db```.
+
+
+In case of 16/18S and ITS you need to train the CREST algorithm with your custom reference database. 
+You may see the relative steps [here](https://hariszaf.github.io/pema_documentation/training_crest_classifier/).
+
+Likewise, for the case of the COI marker gene, you need to train the RDPClassifier and you may see how 
+[here](https://hariszaf.github.io/pema_documentation/training_rdpclassifier/).
--- analysis_directory/phyloseq_in_PEMA.R
+++ analysis_directory/phyloseq_in_PEMA.R
@@ -47,7 +47,7 @@ theme_set(theme_bw())
 
 
 #import the OTU table (or else biotic data)
-biotic <- read.csv("final_table.tsv", sep = "\t", header=TRUE, row.names = 1) 
+biotic <- read.csv("finalTable.tsv", sep = "\t", header=TRUE, row.names = 1) 
 
 #import the tree of the OTUs
 testTree <- "OTUs_tree.tre"
@@ -58,39 +58,39 @@ if (file.exists(testTree)) {
 }
 
 
-#Create taxonomy table from the classification column of the biotic data
+#Create taxonomy table from the Classification column of the biotic data
 
 if (markerGene == 'gene_16S' || markerGene == 'gene_18S') {
 	if (silvaVersion == 'silva_132') {
 	  # Silva 132
-	  taxonomy <- select(biotic, classification)
+	  taxonomy <- select(biotic, Classification)
 	  colnames <- c("Root", "Domain", "Phylum", "Class", "Order", "Family", "Genus", "Species")
-	  taxonomy <- separate(taxonomy, classification, colnames, sep = ";", remove = TRUE,
+	  taxonomy <- separate(taxonomy, Classification, colnames, sep = ";", remove = TRUE,
 						   convert = FALSE, extra = "warn", fill = "warn")
 	  getPalette = colorRampPalette(brewer.pal(8, "Set2"))
 	  
 	} else if (silvaVersion == 'silva_128') {
 	  # Silva 128
-	  taxonomy <- select(biotic, classification)
+	  taxonomy <- select(biotic, Classification)
 	  colnames <- c("Root", "Domain", "Superkingdom", "Superphylum", "Phylum", "Class", "Order", "Family", "Genus", "Species")
-	  taxonomy <- separate(taxonomy, classification, colnames, sep = ";", remove = TRUE,
+	  taxonomy <- separate(taxonomy, Classification, colnames, sep = ";", remove = TRUE,
 							   convert = FALSE, extra = "warn", fill = "warn")
 	  getPalette = colorRampPalette(brewer.pal(10, "Set2"))
 	}	
 	
 } else if (markerGene == 'gene_ITS') {
     # UNITE database
-	taxonomy <- select(biotic, classification)
+	taxonomy <- select(biotic, Classification)
 	colnames <- c("Root", "Superkingdom", "Subgroup", "Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species")
-	taxonomy <- separate(taxonomy, classification, colnames, sep = ";", remove = TRUE,
+	taxonomy <- separate(taxonomy, Classification, colnames, sep = ";", remove = TRUE,
 						 convert = FALSE, extra = "warn", fill = "warn")
 	getPalette = colorRampPalette(brewer.pal(10, "Set2"))
 	
 } else if (markerGene == 'gene_COI') {
 	# MIDORI database
-	taxonomy <- select(biotic, classification)
+	taxonomy <- select(biotic, Classification)
 	colnames <- c("Superkingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species")
-	taxonomy <- separate(taxonomy, classification, colnames, sep = ";", remove = TRUE,
+	taxonomy <- separate(taxonomy, Classification, colnames, sep = ";", remove = TRUE,
 						 convert = FALSE, extra = "warn", fill = "warn")
 	getPalette = colorRampPalette(brewer.pal(7, "Set2"))
 }
@@ -105,8 +105,8 @@ taxonomy_matrix[y] <- "Unknown"
 # prepare the object for the phyloseq object
 TAX = tax_table(taxonomy_matrix)
 
-#remove classification column from biotic data
-biotic <- select(biotic, -classification)
+#remove Classification column from biotic data
+biotic <- select(biotic, -Classification)
 #convert the biotic data from data frame to matrix
 biotic_matrix <- as.matrix(biotic)
 
@@ -336,245 +336,5 @@ bioenv <- bioenv(biotic_transposed, metadata_without_factors)
 summary(bioenv)
 
 
-## Thank you for your patience! :) 
+## You may check your results! 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-###############################        PREVIOUS VERSION
-
-###  SECTION 3
-#
-## create a bar plot, for the taxon rank you like, e.g Phylum 
-## this bar chart is created with the default colour palette of phyloseq
-#barchart <- plot_bar(physeq, fill = "Phylum")
-## in case you think about using ggsave, keep in mind that it has to be exactly after the plot you want to save. 
-##ggsave(filename = "barchart.png", scale = 1, dpi = 100, width = 6)
-#pdf("barchart.pdf", width = 12)
-#print(barchart)
-#dev.off()
-#
-## ATTENTION!
-## in the below commands, we chose the "Habitat" variable from our metadata file as an example.
-## in addition, we chose "Phylum" as an example.
-## you need to set those parameters (metadata variables and taxon level) in order to address your data and your needs.
-#
-## create a bar plot for the 100 most abundant taxa, for the Phylum rank per e.g Habitat (info included in the metadata)
-#top100 <- names(sort(taxa_sums(physeq), decreasing=TRUE))[1:100]
-#physeq.top100 <- transform_sample_counts(physeq, function(OTU) OTU/sum(OTU))
-#physeq.top100 <- prune_taxa(top100, physeq.top100)
-##here you can change the taxonomic rank and the grouping of samples
-#barchart100 <- plot_bar(physeq.top100, fill="Phylum") + facet_wrap(~Habitat, scales="free_x")
-##ggsave(filename = "barchart100.png", scale = 1, dpi = 300)
-#pdf("barchart100.pdf")
-#print(barchart100)
-#dev.off()
-#
-## create a bar plot for all the taxa, for the Phylum rank per e.g. Habitat and Location (info included in the metadata)
-## this bar chart is created with the default colour palette of phyloseq
-#barchart_habitat <- plot_bar(physeq, "Location", fill="Phylum") + facet_wrap(~Habitat, scales="free_x")
-##ggsave(filename = "barchart_habitat.png", scale = 1, dpi = 300, width = 12)
-#pdf("barchart_habitat.pdf", width = 12)
-#print(barchart_habitat)
-#dev.off()
-#
-#
-##get the data frame from the phyloseq object
-#pd <- psmelt(physeq)
-#
-##now let's do the bar plot again, but with custom chosen colours
-##Count how many Phyla are there in your samples
-#HowManyPhyla <- length(unique(unlist(pd[,c("Phylum")])))
-##OR
-#HowManyPhyla <- length(levels(as.factor(pd$Phylum)))
-#
-#
-## Build a colour palette with number of colours as many as 
-## the Phyla in your samples by interpolating the palette "Set2".
-## "Set2" is one of the colorblind friendly palettes
-## Another example of a colorblind friendly palette is "Dark2"
-## If you want, by running the command display.brewer.all(colorblindFriendly = TRUE)
-## you can see all the colorblind friendly palettes of the RColorBrewer package.
-#
-#
-### Now the "getPalette" variable, we set it in the if statement of the marker gene.
-##getPalette = colorRampPalette(brewer.pal(9, "Set2"))
-#PhylaPalette = getPalette(HowManyPhyla)
-#
-##and do the actual plotting
-#barchart_palette <- ggplot(pd, aes(x = Location, y = Abundance, factor(Phylum), fill = factor(Phylum))) +
-#  geom_bar(stat = "identity") +
-#  facet_wrap(~Biome, scales = "free_x") +
-#  scale_fill_manual(values = PhylaPalette) +
-#  guides(fill=guide_legend(ncol=2))
-##ggsave(filename = "barchart_palette.png", scale = 1, dpi = 300, width = 12)
-#pdf("barchart_palette.pdf", width = 12)
-#print(barchart_palette)
-#dev.off()
-#
-## here are some plots only for the case that a tree exists.
-#if ( pointer == 'yes') {
-#	# plot the OTU tree with colour coding by e.g. Habitat (info included in the metadata)
-#	tree1 <- plot_tree(physeq, color="Habitat", label.tips="taxa_names", ladderize="left", justify = "left", size = "Abundance", plot.margin=0.3)
-#	#ggsave(filename = "tree1.png", scale = 1, dpi = 300)
-#	pdf("tree1.pdf")
-#	print(tree1)
-#	dev.off()
-#	
-#	tree2 <- plot_tree(physeq, color="Habitat", label.tips="taxa_names", ladderize="left", plot.margin=0.3)
-#	#ggsave(filename = "tree2.png", scale = 1, dpi = 300)
-#	pdf("tree2.pdf")
-#	print(tree2)
-#	dev.off()
-#	
-#	tree3 <- plot_tree(physeq, color="Habitat", ladderize="left", plot.margin=0.3)
-#	#ggsave(filename = "tree3.png", scale = 1, dpi = 300)
-#	pdf("tree3.pdf")
-#	print(tree3)
-#	dev.off()
-#}
-#
-#
-#	
-## create a heatmap, for the Phylum rank 
-#heatmap <- plot_heatmap(physeq, taxa.label="Phylum")
-##ggsave(filename = "heatmap.png", scale = 1, dpi = 300)
-#pdf("heatmap.pdf")
-#print(heatmap)
-#dev.off()
-#
-## plot the diversity indices with colour coding by e.g. Location (info included in the metadata) 
-#richness <- plot_richness(physeq, measures=c("Shannon", "Simpson", "Chao1"), color="Location")
-##ggsave(filename = "richness.png", scale = 1, dpi = 300, width = 12)
-#pdf("richness.pdf", width = 12)
-#print(richness)
-#dev.off()
-#
-## create the nmds plot colour coded by e.g. Location (info included in the metadata) 
-#ord.nmds.bray <- ordinate(physeq, method="NMDS", distance="bray")
-#p1 <- plot_ordination(physeq, ord.nmds.bray, color="Location", title="Bray NMDS")
-##ggsave(filename = "p1.png", scale = 1, dpi = 300)
-#pdf("p1.pdf")
-#print(p1)
-#dev.off()
-#
-## Split the previous nmds plot per e.g. Habitat (info included in the metadata) 
-#p2 <- p1 + facet_wrap(~Habitat, 3)
-##ggsave(filename = "p2.png", scale = 1, dpi = 300)
-#pdf("p2.pdf")
-#print(p2)
-#dev.off()
-#
-## Create an nmds plot per Phylum
-#p3 <- plot_ordination(physeq, ord.nmds.bray, type="taxa", color="Phylum", title="Bray NMDS")
-##ggsave(filename = "p3.png", scale = 1, dpi = 300, width = 12)
-#pdf("p3.pdf", width = 12)
-#print(p3)
-#dev.off()
-#
-## Split an nmds plot per Phylum
-#p4 <- p3 + facet_wrap(~Phylum, 3)
-##ggsave(filename = "p4.png", scale = 1, dpi = 300, width = 20)
-#pdf("p4.pdf", width = 20)
-#print(p4)
-#dev.off()
-#
-##Run PERMANOVA to check for significance based on the Habitat factor (info included in the metadata) 
-#OTU.adonis.Habitat <- adonis(biotic_transposed ~ metadata$Habitat, data=metadata, permutations = 999, distance = "bray")
-#OTU.adonis.Habitat
-#
-##Run PERMANOVA to check for significance based on the combination (+) of Location & Habitat factors
-#OTU.adonis <- adonis(biotic_transposed ~ metadata$Location+metadata$Habitat,data=metadata,permutations = 999,distance = "bray") 
-##summary(OTU.adonis)
-#OTU.adonis
-#
-## Create a bar plot only for the Proteobacteria
-#physeq.proteobacteria = subset_taxa(physeq, Phylum=="Proteobacteria")
-#proteo1 <- plot_bar(physeq.proteobacteria, title = "Proteobacteria per sample")
-##ggsave(filename = "proteo1.png", scale = 1, dpi = 300)
-#pdf("proteo1.pdf")
-#print(proteo1)
-#dev.off()
-#
-#proteo2 <- plot_bar(physeq.proteobacteria, "Class", "Abundance", "Habitat", title = "Proteobacteria per class, colour by Habitat")
-##ggsave(filename = "proteo2.png", scale = 1, dpi = 300)
-#pdf("proteo2.pdf")
-#print(proteo2)
-#dev.off()
-#
-#proteo3 <- plot_bar(physeq.proteobacteria, "Habitat", title = "Proteobacteria per Habitat")
-##ggsave(filename = "proteo3.png", scale = 1, dpi = 300)
-#pdf("proteo3.pdf")
-#print(proteo3)
-#dev.off()
-#
-#proteo4 <- plot_bar(physeq.proteobacteria, "Class", title = "Proteobacteria per class")
-##ggsave(filename = "proteo4.png", scale = 1, dpi = 300)
-#pdf("proteo4.pdf")
-#print(proteo4)
-#dev.off()
-#
-#proteo5 <- plot_bar(physeq.proteobacteria, "Class", "Abundance", "Class", facet_grid="Habitat~.", title = "Proteobacteria per Class, per Habitat")
-##ggsave(filename = "proteo5.png", scale = 1, dpi = 300, width = 12)
-#pdf("proteo5.pdf", width = 12)
-#print(proteo5)
-#dev.off()
-#
-##BIO-ENV
-## select the columns of the metadata file that contain numeric data
-#metadata_without_factors <- select_if(metadata, is.numeric)
-## run the bioenv test
-#bioenv <- bioenv(biotic_transposed, metadata_without_factors) 
-## display the results
-#summary(bioenv)
-#
-### Thank you for your patience! :) 
-#
-#
