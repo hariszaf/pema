20fb5bf3744e2e0bd289e53e49eb69edfb286914
hariszaf <haris.zafr@gmail.com>
2021-11-25 03:42:52 +0200

    runs

--- pema_docker_image/sanity_check/run_sannity_check.sh
+++ pema_docker_image/sanity_check/run_sannity_check.sh
@@ -8,53 +8,53 @@
 CWD=$(pwd)
 
 ## Run example case for 16S using vsearch
-rm -rf test_16S*
+rm -rf 16S/test_16S*
 cp 16S/parameters_vsearch.tsv 16S/parameters.tsv
 docker run --rm -v $CWD/16S/:/mnt/analysis/ hariszaf/pema:v.2.1.4 ./pema_latest.bds
 rm 16S/parameters.tsv
 
-## Run example case for 16S using Swarm
-cp 16S/parameters_swarm.tsv 16S/parameters.tsv
-docker run --rm -v $CWD/16S/:/mnt/analysis/ hariszaf/pema:v.2.1.4 ./pema_latest.bds
-rm 16S/parameters.tsv
+# ## Run example case for 16S using Swarm
+# cp 16S/parameters_swarm.tsv 16S/parameters.tsv
+# docker run --rm -v $CWD/16S/:/mnt/analysis/ hariszaf/pema:v.2.1.4 ./pema_latest.bds
+# rm 16S/parameters.tsv
 
-#----------------------------
+# #----------------------------
 
-##  Run example case for 18S using vsearch and Silva db
-rm -rf test_18S*
-cp 18S/parameters_vsearch.tsv 18S/parameters.tsv
-docker run --rm -v $CWD/18S/:/mnt/analysis/ hariszaf/pema:v.2.1.3 ./pema_latest.bds
-rm 18S/parameters.tsv
+# ##  Run example case for 18S using vsearch and Silva db
+# rm -rf test_18S*
+# cp 18S/parameters_vsearch.tsv 18S/parameters.tsv
+# docker run --rm -v $CWD/18S/:/mnt/analysis/ hariszaf/pema:v.2.1.3 ./pema_latest.bds
+# rm 18S/parameters.tsv
 
-## Run example case for 18S using Swarm and Silva db
-cp 18S/parameters_swarm.tsv 18S/parameters.tsv
-docker run --rm -v $CWD/18S/:/mnt/analysis/ hariszaf/pema:v.2.1.4 ./pema_latest.bds
-rm 18S/parameters.tsv
+# ## Run example case for 18S using Swarm and Silva db
+# cp 18S/parameters_swarm.tsv 18S/parameters.tsv
+# docker run --rm -v $CWD/18S/:/mnt/analysis/ hariszaf/pema:v.2.1.4 ./pema_latest.bds
+# rm 18S/parameters.tsv
 
-## Run example case for 18S using vsearcg and PR2 db
-cp 18S/parameters_vsearch_pr2.tsv 18S/parameters.tsv
-docker run --rm -v $CWD/18S/:/mnt/analysis/ hariszaf/pema:v.2.1.4 ./pema_latest.bds
-rm 18S/parameters.tsv
-
-
-#----------------------------
-
-## Run example case for COI using Swarm and Midori-1
-rm -rf test_COI*
-cp COI/parameters_swarm.tsv 18S/parameters.tsv
-docker run --rm -v $CWD/COI/:/mnt/analysis/ hariszaf/pema:v.2.1.3 ./pema_latest.bds
-rm COI/parameters.tsv
+# ## Run example case for 18S using vsearcg and PR2 db
+# cp 18S/parameters_vsearch_pr2.tsv 18S/parameters.tsv
+# docker run --rm -v $CWD/18S/:/mnt/analysis/ hariszaf/pema:v.2.1.4 ./pema_latest.bds
+# rm 18S/parameters.tsv
 
 
-## Run example case for COI using Swarm and a custom db (Amvrakikos)
+# #----------------------------
+
+# ## Run example case for COI using Swarm and Midori-1
+# rm -rf test_COI*
+# cp COI/parameters_swarm.tsv 18S/parameters.tsv
+# docker run --rm -v $CWD/COI/:/mnt/analysis/ hariszaf/pema:v.2.1.3 ./pema_latest.bds
+# rm COI/parameters.tsv
+
+
+# ## Run example case for COI using Swarm and a custom db (Amvrakikos)
 
 
 
 
-#----------------------------
+# #----------------------------
 
-## Run example case for ITS using Swarm and Unite
-docker run --rm -v $CWD/ITS/:/mnt/analysis/ hariszaf/pema:v.2.1.3 ./pema_latest.bds
+# ## Run example case for ITS using Swarm and Unite
+# docker run --rm -v $CWD/ITS/:/mnt/analysis/ hariszaf/pema:v.2.1.3 ./pema_latest.bds
 
 
 
