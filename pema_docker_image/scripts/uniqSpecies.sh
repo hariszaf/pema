5da9d39e70d4fd72416dfd3aa01ef23d3a7b14d7
hariszaf <haris-zaf@hcmr.gr>
2021-11-26 16:30:55 +0000

    slight modifs

--- pema_docker_image/scripts/uniqSpecies.sh
+++ pema_docker_image/scripts/uniqSpecies.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-sed 's/1\./10/g; s/0\.//g' tax_assign_swarm_COI.txt > temp.txt
+# Percentage of confidence level 
+confidence_score=${1}
 
+sed 's/1\./10/g; s/0\.//g' tax_assignments.tsv > temp.txt
 #awk '{for(i = 1 ; i <= NF ; i++) {if ($i ~ /^[0-9]+$/ && $i <= 97) { next }}} 1'  < temp.txt > lesna.txt
 
-awk '{for(i = 1 ; i <= NF ; i++) {
-if ($i ~ /^[0-9]+$/ && $i < 97) { next };
-     }
+awk -v "percentage=${confidence_score}}" '{
+    for(i = 1 ; i <= NF ; i++) {
+        if ($i ~ /^[0-9]+$/ && $i > $percentage) { next };
+    }
 } 1'  < temp.txt > lesna.txt
 
-
-awk '{n = 4; for (--n; n >= 0; n--){ printf "%s\t",$(NF-n)} print ""}' lesna.txt > almost.txt
-
+awk '{n = 4; for (--n; n >= 0; n--){ print$1 printf "%s\t",$(NF-n)} print ""}' lesna.txt > almost.txt
 awk '{NF-=2}1' almost.txt > species.txt
 
 cat species.txt | sort | uniq | sort > uniq_species.txt
 
 rm temp.txt lesna.txt almost.txt species.txt
+
+
