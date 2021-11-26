d8b9f4e2eb63fbb8dd36f734c1b32d8d7a2bf336
hariszaf <haris-zaf@hcmr.gr>
2021-11-26 00:24:57 +0000

    edit parameter files; sequencerPrefix parameter was added

--- pema_docker_image/sanity_check/run_sannity_check.sh
+++ pema_docker_image/sanity_check/run_sannity_check.sh
@@ -17,16 +17,23 @@ CWD=$(pwd)
 # mv 16S/mydata/ERR* 16S/ena_data
 # echo "PEMA for 16S using vsearch and Silva db has been completed"
 
-# ## Run example case for 16S non ENA data using vsearch
-# echo "PEMA for 16S in non ENA format using vsearch and Silva db is about to start"
-# cp 16S/parameters_vsearch_noENA_data.tsv 16S/parameters.tsv
-# mv 16S/noEna_data/We1* 16S/mydata/
-# docker run --rm -v $CWD/16S/:/mnt/analysis/ hariszaf/pema:v.2.1.4 rm -rf /mnt/analysis/test_16S*
-# docker run --rm -v $CWD/16S/:/mnt/analysis/ hariszaf/pema:v.2.1.4 ./pema_latest.bds
-# rm 16S/parameters.tsv
-# mv 16S/initial_data/We1* 16S/noEna_data/
-# rm 16S/mydata/ERR*
-# echo "PEMA for 16S using vsearch and Silva db has been completed"
+## Run example case for 16S non ENA data using vsearch
+echo "PEMA for 16S in non ENA format using vsearch and Silva db is about to start"
+cp 16S/parameters_vsearch_noENA_data.tsv 16S/parameters.tsv
+
+docker run --rm -v $CWD/16S/:/mnt/analysis/ hariszaf/pema:v.2.1.4 rm -rf /mnt/analysis/test_16S_vsearch_noENA/ 
+docker run --rm -v $CWD/16S/:/mnt/analysis/ hariszaf/pema:v.2.1.4 rm -rf /mnt/analysis/initial_data/ 
+docker run --rm -v $CWD/16S/:/mnt/analysis/ hariszaf/pema:v.2.1.4 rm -rf /mnt/analysis/mydata/
+docker run --rm -v $CWD/16S/:/mnt/analysis/ hariszaf/pema:v.2.1.4 rm /mnt/analysis/mapping_files_for_PEMA.tsv
+
+mkdir 16S/mydata
+cp 16S/noEna_data/We1* 16S/mydata/
+
+docker run --rm -v $CWD/16S/:/mnt/analysis/ hariszaf/pema:v.2.1.4 ./pema_latest.bds 
+
+rm 16S/parameters.tsv
+echo "PEMA for 16S in non ENA format using vsearch and Silva db has been completed"
+
 
 # ## Run example case for 16S using Swarm
 # echo "PEMA for 16S using Swarm and Silva db is about to start"
@@ -38,13 +45,13 @@ CWD=$(pwd)
 
 # # #----------------------------
 
-#  Run example case for 18S using vsearch and Silva db
-echo "PEMA for 18S using vsearch and Silva db is about to start"
-cp 18S/parameters_vsearch.tsv 18S/parameters.tsv
-docker run --rm -v $CWD/18S/:/mnt/analysis/ hariszaf/pema:v.2.1.4 rm -rf /mnt/analysis/test_18S_vsearch/
-docker run --rm -v $CWD/18S/:/mnt/analysis/ hariszaf/pema:v.2.1.4 ./pema_latest.bds
-rm 18S/parameters.tsv
-echo "PEMA for 18S using vsearch and Silva db has been completed"
+# #  Run example case for 18S using vsearch and Silva db
+# echo "PEMA for 18S using vsearch and Silva db is about to start"
+# cp 18S/parameters_vsearch.tsv 18S/parameters.tsv
+# docker run --rm -v $CWD/18S/:/mnt/analysis/ hariszaf/pema:v.2.1.4 rm -rf /mnt/analysis/test_18S_vsearch/
+# docker run --rm -v $CWD/18S/:/mnt/analysis/ hariszaf/pema:v.2.1.4 ./pema_latest.bds
+# rm 18S/parameters.tsv
+# echo "PEMA for 18S using vsearch and Silva db has been completed"
 
 # ## Run example case for 18S using Swarm and Silva db
 # echo "PEMA for 18S using Swarm and Silva db is about to start"
