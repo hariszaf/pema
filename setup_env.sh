32dede93a33bbda823fe33d6bcf0a5af48013433
Haris Zafeiropoulos <haris.zafeiropoulos@kuleuven.be>
2025-07-05 16:19:47 +0200

    removing threads, persample; installing phylogeny based tax assign tools

--- setup_env.sh
+++ setup_env.sh
@@ -22,7 +22,8 @@ VERSION_ARG=false
 SHELL_CONFIG="$HOME/.bashrc"
 
 # Parse options using getopt
-PARSED=$(getopt --options kh --long kofam,help -- "$@")
+# PARSED=$(getopt --options eh --long env,help -- "$@")
+PARSED=$(getopt --options eh --long env:,help -- "$@")
 if [[ $? -ne 0 ]]; then
   echo "❌ Failed to parse options." >&2
   exit 1
@@ -32,15 +33,16 @@ fi
 eval set -- "$PARSED"
 
 # Loop through options
+# Parse options
 while true; do
   case "$1" in
-    # -k|--kofam)
-    #   KOFAM_ARG=true
-    #   shift
-    #   ;;
     -h|--help)
       HELP_ARG=true
       shift
+      ;;
+    -e|--env)
+      RUN_ENV="$2"
+      shift 2
       ;;
     --)
       shift
@@ -53,12 +55,24 @@ while true; do
   esac
 done
 
+# Validate ENV value and that --env value is provided
+if ! $HELP_ARG && [[ -z "$RUN_ENV" ]]; then
+  echo -e "$RED_CROSS Error: --env is required."
+  exit 1
+fi
+
+if [[ -n "$RUN_ENV" && "$RUN_ENV" != "local" && "$RUN_ENV" != "container" ]]; then
+  echo -e "$RED_CROSS Error: --env must be either 'local' or 'container'."
+  exit 1
+fi
+
 # 
 echo -e "\n Building conda environment and installing required dependencies to enable pema ${ROCKET} \n\n"
 
 if $HELP_ARG; then
   echo "Usage: bash setup_env.sh [options]"
   echo "  -h, --help     Show this help message"
+  echo "  -e, --env      Running environment [container, local]"
   echo ""
   echo -e "${RED_CIRCLE} Either conda or miniconda is considered to be available. If not, setup_environment.sh will fail."
   echo -e "${RED_CIRCLE} Make sure you run the script from the root folder of the pema repository."
@@ -132,7 +146,9 @@ chmod +x "$CONDA_PREFIX/etc/conda/deactivate.d/env_vars.sh"
 
 
 # Build a hidden folder for pema-related software
-INSTALL_DIR=$HOME/.pema/
+INSTALL_DIR=$HOME/.pema
+
+echo $INSTALL_DIR
 mkdir -p "$INSTALL_DIR"
 cd "$INSTALL_DIR"
 
@@ -146,15 +162,15 @@ if [[ -x "$BDS_DIR/bds" ]]; then
   echo -e "$GREEN_TICK bds is already installed at $BDS_DIR"
 else
   echo -e "$HOURGLASS Installing bds to $BDS_DIR..."
-  cp $SETUP_WD/pema_docker_image/pemabase/tools/bds_Linux.tgz "$BDS_TGZ"
+  cp $SETUP_WD/pema/ext_data/bds_Linux.tgz "$BDS_TGZ"
   tar -xvzf bds_*.tgz
   rm -f "$BDS_TGZ"
   echo -e "BigDataScript programming language was installed at: $BDS_DIR  $TADA"
 fi
 
 # Add $INSTALL_DIR/.bds to PATH if not already included
-if [[ ":$PATH:" != *":$HOME/.pema/.bds:"* ]]; then
-  echo 'export PATH="$HOME/.pema/.bds:$PATH"' >> $SHELL_CONFIG
+if [[ ":$PATH:" != *":$INSTALL_DIR/.bds:"* ]]; then
+  echo 'export PATH="$INSTALL_DIR/.bds:$PATH"' >> $SHELL_CONFIG
   echo -e "Added ${INSTALL_DIR}/.bds to PATH. Restart your terminal or run: source ~/.bashrc $TADA"
 fi
 
@@ -169,6 +185,7 @@ fi
 SWARM=3.1.5        # NOTE: note used so far on the installation process, i.e. would change without noticing
 SPADES=3.14.0
 RDPCLASSIFIER=2.14
+RAXML=1.2.2
 CREST_PEMA="https://zenodo.org/record/5734317/files/crest.tar.gz"
 
 
@@ -178,7 +195,7 @@ CREST_PEMA="https://zenodo.org/record/5734317/files/crest.tar.gz"
 if [[ -x $INSTALL_DIR/fastp ]]; then
   echo -e "$GREEN_TICK fastp already exists at $INSTALL_DIR/fastp"
 else
-  echo -e "$HOURGLASS fastp not found in $INSTALL_DIR/. Downloading..."
+  echo -e "$HOURGLASS fastp not found in $INSTALL_DIR. Downloading..."
   wget http://opengene.org/fastp/fastp -O $INSTALL_DIR/fastp && \
   chmod a+x $INSTALL_DIR/fastp && \
   echo -e "$GREEN_TICK fastp downloaded to $INSTALL_DIR/fastp"
@@ -189,7 +206,7 @@ fi
 # Install fastQC
 # --------------
 if [[ -x $INSTALL_DIR/FastQC ]]; then
-  echo -e "$GREEN_TICK fasQC already exists at $INSTALL_DIR/FastQC"
+  echo -e "$GREEN_TICK fastQC already exists at $INSTALL_DIR/FastQC"
 else
   wget https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.8.zip 
   unzip fastqc_v0.11.8.zip
@@ -199,8 +216,8 @@ else
 fi
 
 # Add $INSTALL_DIR/FastQC/fastqc to PATH if not already included
-if [[ ":$PATH:" != *":$HOME/.pema/FastQC:"* ]]; then
-  echo 'export PATH="$HOME/.pema/FastQC:$PATH"' >> $SHELL_CONFIG
+if [[ ":$PATH:" != *":$INSTALL_DIR/FastQC:"* ]]; then
+  echo 'export PATH="$INSTALL_DIR/FastQC:$PATH"' >> $SHELL_CONFIG
   echo -e "Added $INSTALL_DIR/FastQC to PATH. Restart your terminal or run: source ~/.bashrc $TADA"
 fi
 
@@ -216,7 +233,7 @@ else
   cd obitools4
   wget -O -L  https://raw.githubusercontent.com/metabarcoding/obitools4/master/install_obitools.sh 
   bash install_obitools.sh -i .
-  echo 'export PATH="$HOME/.pema/obitools4/bin:$PATH"' >> $SHELL_CONFIG
+  echo 'export PATH="$INSTALL_DIR/obitools4/bin:$PATH"' >> $SHELL_CONFIG
   echo -e "Obitools4 is now added on PATH $TADA"
   cd ..
 fi
@@ -236,8 +253,8 @@ else
 fi
 
 # Add $INSTALL_DIR/vsearch to PATH if not already included
-if [[ ":$PATH:" != *":$HOME/.pema/vsearch/bin:"* ]]; then
-  echo 'export PATH="$HOME/.pema/vsearch/bin:$PATH"' >> $SHELL_CONFIG
+if [[ ":$PATH:" != *":$INSTALL_DIR/search/bin:"* ]]; then
+  echo 'export PATH="$INSTALL_DIR/vsearch/bin:$PATH"' >> $SHELL_CONFIG
   echo -e "Added $INSTALL_DIR/vsearch/bin to PATH. Restart your terminal or run: source ~/.bashrc $TADA"
 fi
 
@@ -268,9 +285,9 @@ else
 fi
 
 # Add $INSTALL_DIR/SPAdes to PATH if not already included
-if [[ ":$PATH:" != *":$HOME/.pema/SPAdes-${SPADES}-Linux/bin:"* ]]; then
-  # echo 'export PATH="$HOME/.pema/SPAdes-${SPADES}-Linux/bin:$PATH"' >> $SHELL_CONFIG
-  echo "export PATH=\"\$HOME/.pema/SPAdes-${SPADES}-Linux/bin:\$PATH\"" >> "$SHELL_CONFIG"
+if [[ ":$PATH:" != *":$INSTALL_DIR/SPAdes-${SPADES}-Linux/bin:"* ]]; then
+  # echo 'export PATH="$INSTALL_DIRSPAdes-${SPADES}-Linux/bin:$PATH"' >> $SHELL_CONFIG
+  echo "export PATH=\"\$INSTALL_DIR/SPAdes-${SPADES}-Linux/bin:\$PATH\"" >> "$SHELL_CONFIG"
   echo -e "Added $INSTALL_DIR/SPAdes-$SPADES-Linux/bin to PATH. Restart your terminal or run: source ~/.bashrc $TADA"
 fi
 
@@ -293,8 +310,8 @@ else
 fi
 
 # Add $INSTALL_DIR/PANDAseq to PATH if not already included
-if [[ ":$PATH:" != *":$HOME/.pema/PANDAseq/bin:"* ]]; then
-  echo 'export PATH="$HOME/.pema/PANDAseq/bin:$PATH"' >> $SHELL_CONFIG
+if [[ ":$PATH:" != *":$INSTALL_DIR/PANDAseq/bin:"* ]]; then
+  echo 'export PATH="$INSTALL_DIR/PANDAseq/bin:$PATH"' >> $SHELL_CONFIG
   echo -e "Added $INSTALL_DIR/PANDAseq/bin to PATH. Restart your terminal or run: source ~/.bashrc $TADA"
 fi
 
@@ -312,8 +329,8 @@ else
 fi
 
 # Add $INSTALL_DIR/swarm to PATH if not already included
-if [[ ":$PATH:" != *":$HOME/.pema/swarm/bin:"* ]]; then
-  echo 'export PATH="$HOME/.pema/swarm/bin:$PATH"' >> $SHELL_CONFIG
+if [[ ":$PATH:" != *":$INSTALL_DIR/swarm/bin:"* ]]; then
+  echo 'export PATH="$INSTALL_DIR/swarm/bin:$PATH"' >> $SHELL_CONFIG
   echo -e "Added $INSTALL_DIR/swarm/bin to PATH. Restart your terminal or run: source ~/.bashrc $TADA"
 fi
 
@@ -332,8 +349,8 @@ else
 fi
 
 # Add $INSTALL_DIR/ncbi-blast-2.8.1+/bin to PATH if not already included
-if [[ ":$PATH:" != *":$HOME/.pema/ncbi-blast-2.8.1+/bin:"* ]]; then
-  echo 'export PATH="$HOME/.pema/ncbi-blast-2.8.1+/bin:$PATH"' >> $SHELL_CONFIG
+if [[ ":$PATH:" != *":$INSTALL_DIR/ncbi-blast-2.8.1+/bin:"* ]]; then
+  echo 'export PATH="$INSTALL_DIR/ncbi-blast-2.8.1+/bin:$PATH"' >> $SHELL_CONFIG
   echo -e "Added $INSTALL_DIR/swarm/bin to PATH. Restart your terminal or run: source ~/.bashrc $TADA"
 fi
 
@@ -356,35 +373,108 @@ fi
 
 
 if [[ -x $INSTALL_DIR/rdp_classifier_$RDPCLASSIFIER ]]; then
+
   echo -e "$GREEN_TICK RDP Classifier is already installed"
+
 else
 
   echo -e "$HOURGLASS RDP Classifier is being installed.. "
   wget -L -O rdp_classifier_$RDPCLASSIFIER.zip "https://sourceforge.net/projects/rdp-classifier/files/rdp-classifier/rdp_classifier_$RDPCLASSIFIER.zip/download"
   unzip rdp_classifier_$RDPCLASSIFIER.zip
   rm rdp_classifier_$RDPCLASSIFIER.zip
+
+  echo 'export PATH="$INSTALL_DIR/rdp_classifier_$RDPCLASSIFIER/"' >> $SHELL_CONFIG
   echo -e "RDP Classifier has been installed $TADA"
 
-  echo 'export PATH="$HOME/.pema/rdp_classifier_$RDPCLASSIFIER/"'
 fi
 
 
-if [[ -x $INSTALL_DIR/rdp_classifier_$RDPCLASSIFIER/TRAIN ]]; then
+RDP_TRAIN_DIR="$INSTALL_DIR/rdp_classifier_$RDPCLASSIFIER/TRAIN"
+
+if [[ -d "$RDP_TRAIN_DIR" ]] && [ "$(ls -A "$RDP_TRAIN_DIR")" ]; then
+
   echo -e "$GREEN_TICK PEMA trained databases to be use with RDP Classifier have been already retrieved." 
+
 else
+
   echo -e "$HOURGLASS Download PEMA trained RDP databases..."
+
   cd rdp_classifier_$RDPCLASSIFIER/
 
   # TODO: REPLACE THE URL  
-  wget wget -O TRAIN.zip -L  <URL>
-
+  # wget -O TRAIN.zip -L  <URL>
   unzip TRAIN.zip
-
   rm TRAIN.zip
-
-  cp $INSTALL_DIR/rdp_classifier_$RDPCLASSIFIER/TRAIN/midori_1/rRNAClassifier.properties $INSTALL_DIR/rdp_classifier_$RDPCLASSIFIER/TRAIN/12S_v2.0.0/rRNAClassifier.properties
+  cp $RDP_TRAIN_DIR/midori_1/rRNAClassifier.properties $RDP_TRAIN_DIR/12S_v2.0.0/rRNAClassifier.properties
 
   echo -e "PEMA trained RDP databases were retrieved $TADA"
+fi
+
+# -----------------
+# Phylogeny based taxonomy assignment
+# -----------------
+
+# -----------------
+# PaPaRa
+# -----------------
+if [[ -x $INSTALL_DIR/papara_static_x86_64 ]]; then
+  echo -e "$GREEN_TICK PaPaRa is already available." 
+else
+  echo -e "$HOURGLASS Download PaPaRa ..."
+  wget https://sco.h-its.org/exelixis/resource/download/software/papara_nt-2.5-static_x86_64.tar.gz
+  # The untar returns a papara_static_x86_64 file which is to be used 
+  tar -zxvf papara_nt-2.5-static_x86_64.tar.gz 
+  rm papara_nt-2.5-static_x86_64.tar.gz
+fi
+
+# -----------------
+# EPA-ng
+# -----------------
+if [[ -x $INSTALL_DIR/epa/ ]]; then
+
+  echo -e "$GREEN_TICK Evolutionary Placement Algorithm (EPA)-ng is already available." 
+
+else
+
+  echo -e "$HOURGLASS Download Evolutionary Placement Algorithm EPA-ng ..."
+
+  git clone --recursive https://github.com/Pbdas/epa.git
+
+  if [[ "$RUN_ENV" == "local" ]]; then
+
+    if ! sudo apt-get install --yes autotools-dev flex bison automake; then
+      echo -e "$RED_CROSS Warning: Evolutionary Placement Algorithm installation failed. \
+            PEMA will not be able to run phylogeny-based taxonomy assignmnet. \
+            Make sure you are running this as root to get this option. PEMA installation will now continue anyway..."
+    fi
+
+  else 
+
+    apt-get install --"yes" autotools-dev flex bison automake
+
+  fi
+
+  # Make 
+  cd epa
+  make
+  cd ..
+
+  echo 'export PATH="$INSTALL_DIR/epa/bin/"' >> $SHELL_CONFIG
+  echo -e "EPA has been installed $TADA"
+fi
+
+
+# -----------------
+# RAxML-ng
+# -----------------
+if [[ -x $INSTALL_DIR/raxml-ng ]]; then
+  echo -e "$GREEN_TICK RAxML-ng already available"
+else
+  echo -e "$HOURGLASS Download RAxML-ng binary.."
+  wget https://github.com/amkozlov/raxml-ng/releases/download/${RAXML}/raxml-ng_v${RAXML}_linux_x86_64.zip
+  unzip raxml-ng_v${RAXML}_linux_x86_64.zip 
+  rm raxml-ng_v${RAXML}_linux_x86_64.zip
+  echo -e "$TADA RAxML-ng binary has been downloaded."
 fi
 
 
@@ -395,11 +485,9 @@ fi
 pip install crest4
 pip install ncbi-taxonomist
 
-
-
 # Add $INSTALL_DIR to PATH if not already included
-if [[ ":$PATH:" != *":$HOME/.pema:"* ]]; then
-  echo 'export PATH="$HOME/.pema:$PATH"' >> $SHELL_CONFIG
+if [[ ":$PATH:" != *":$INSTALL_DIR/:"* ]]; then
+  echo 'export PATH="$INSTALL_DIR/:$PATH"' >> $SHELL_CONFIG
   echo -e "Added $INSTALL_DIR to PATH. Restart your terminal or run: source ~/.bashrc $TADA"
 fi
 
@@ -421,5 +509,13 @@ else
     echo -e "$GREEN_TICK PEMA_HOME already set in $SHELL_CONFIG"
 fi
 
+
+cd $INSTALL_DIR
+if ls *.tar.gz *.zip 1> /dev/null 2>&1; then
+  echo "Files found, proceeding with removal."
+  rm *.tar.gz *.zip
+fi
+
+echo -e "\n\n $TADA PEMA installation has been completed successfully!\n"
 
 source $SHELL_CONFIG
